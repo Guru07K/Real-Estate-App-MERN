a0799a76eed1eddc54cd179b93b72a2f5c49a7fc
gnanaguru <gnanagurubb55@gmail.com>
2024-06-26 22:54:46 +0530

    Create Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,13 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import Signin from './pages/Signin'
  import SignUp from './pages/SignUp'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={ <Home/>}/>
         <Route path='/about' element={ <About/>}/>
@@ -16,6 +19,7 @@ const App = () => {
         <Route path='/signup' element={ <SignUp/>}/>
         <Route path='/profile' element={ <Profile/>}/>
       </Routes>
+    <Footer/>
     </BrowserRouter>
   ) 
 }
